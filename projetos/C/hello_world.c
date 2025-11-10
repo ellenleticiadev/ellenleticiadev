a26dd5ad4437faca9f03b915eb233e95db66b579
EllenLeticia2006 <ellenleticiacorreiabarros@gmail.com>
2025-11-10 20:17:47 -0300

    bovo

---
+++ projetos/C/hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Programa Hello World em C
+int main() {
+    printf("Olá, Mundo! Bem-vindo ao meu perfil!\n");
+    printf("Programação em C é incrível!\n");
+    
+    return 0;
+}
---
+++ projetos/Python/hello_world.py
@@ -0,0 +1,8 @@
+# Programa Hello World em Python
+
+def main():
+    print("Olá, Mundo! Bem-vindo ao meu perfil!")
+    print("Programação em Python é incrível!")
+
+if __name__ == "__main__":
+    main()
